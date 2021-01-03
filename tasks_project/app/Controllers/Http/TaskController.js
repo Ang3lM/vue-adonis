7c6b621fd56f04da82cd78f62fb9c86f38241855
Ang3lM <angellara5a2015@gmail.com>
2021-01-02 22:28:34 -0400

    [IMP] TaskController.js Creacion del metodo crear para crear las tareas a un project

--- tasks_project/app/Controllers/Http/TaskController.js
+++ tasks_project/app/Controllers/Http/TaskController.js
@@ -1,6 +1,25 @@
 'use strict'
 
+const Task = use('App/Models/Task');
+const Project = use('App/Models/Project');
+const AuthorizationService = use('App/Services/AuthorizationService');
+
 class TaskController {
+	async create({auth, request, params}){
+		const user = await auth.getUser();
+		var {description} = request.all();
+		var {id} = request.params;
+		var project =  await Project.find(id);
+		var task = new Task();
+		AuthorizationService.verifyPermission(project, user);
+		task.fill({
+			description
+		});
+		await project.tasks().save(task);
+		return task;
+
+
+	}
 }
 
 module.exports = TaskController
