d90d23f1d6aa838624ce68de4931add7b09cc971
Ang3lM <angellara5a2015@gmail.com>
2021-01-02 21:32:04 -0400

    [ADD]TaskController.js y creacion del endpoint de crear para el controller task

--- tasks_project/start/routes.js
+++ tasks_project/start/routes.js
@@ -20,9 +20,12 @@ Route.group(()=>{
 
 	Route.post('auth/register', 'UserController.register');
 	Route.post('auth/login', 'UserController.login');
+
 	Route.get('projects', 'ProjectController.index').middleware('auth');
 	Route.post('projects', 'ProjectController.create').middleware('auth');
 	Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth');
 	Route.patch('projects/:id', 'ProjectController.update').middleware('auth');
+
+	Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth');
 }).prefix('api');
 
